c5f8b1776f1aa36c612a6d71c434cff2569d2fd5
sbh0914 <sbh0636@naver.com>
2023-05-14 13:46:30 +0900

    quit without error
    에러 발생하지 않고 시스템 종료

--- Server_Python/gitserver.py
+++ Server_Python/gitserver.py
@@ -4,9 +4,12 @@ import threading
 import sys
 import asyncio
 def send_to_all(msg):
-    for sock in user_list:
-        sock.send(msg.encode('euc-kr'))
-        
+    if not clients:  # 클라이언트가 없는 경우
+        print("접속자가 없습니다.")
+    else:
+        for sock in user_list:
+            sock.send(msg.encode('euc-kr'))
+
 def off(sock):
     
     # send_to_all(user_list[sock][0]+'님이 나갔습니다.')
@@ -18,10 +21,16 @@ def send_message():
     while True:
         msg = input()
         if(msg=="quit"):
-            listeningSock.close()
+            global server_running
+            server_running = False
+            send_to_all("서버가 종료됩니다.")  # 서버 종료 알림 메시지를 전송
+            # time.sleep(2)  # 메시지를 보낸 후에 2초 대기
+            for client in clients:  # 모든 클라이언트 소켓을 닫음
+                client.close()
+            listeningSock.close()  # 서버 소켓을 닫음
             sys.exit()
         else:
-            listeningSock.send_to_all(msg.encode('euc-kr'))
+            send_to_all(msg)
 
 # def outid_message(outid):
 #     send_to_all('{}이(가) 나갔습니다.'.format(outid))
@@ -51,7 +60,10 @@ def recv_msg(sock):
                 newsoc = clients[a-1]
                 send_to_all('{}이(가) 나갔습니다.'.format(outid))
             break
-
+        except ConnectionAbortedError:
+            # ConnectionAbortedError 예외 처리 추가
+            print("서버가 종료되었습니다.")
+            break
 def handle_client(serviceSock, addr):
     print(str(addr), '에서 접속되었습니다.')
     while True:
@@ -61,12 +73,10 @@ def handle_client(serviceSock, addr):
             break
         else:
             serviceSock.send('중복'.encode('euc-kr'))
-    send_to_all(nickname+"님이 접속하였습니다.")
-    
     clients.append(serviceSock)
     nicknames.append(nickname)
     user_list[serviceSock] = (nickname, addr)
-
+    send_to_all(nickname+"님이 접속하였습니다.")
     recv_thread = threading.Thread(target=recv_msg, args=(serviceSock,))
     recv_thread.start()
     
@@ -84,7 +94,12 @@ send_all.start()
 clients=[]
 nicknames = []
 user_list = {}
-while True:
-    serviceSock, addr = listeningSock.accept()
+server_running = True  # 서버가 실행중인지 확인하는 플래그 변수
+
+while server_running:
+    try:
+        serviceSock, addr = listeningSock.accept()
+    except OSError:  # 서버 소켓이 닫히면 OSError가 발생합니다.
+        break  # 에러가 발생하면 루프를 빠져나와 서버를 종료합니다.
     client_thread = threading.Thread(target=handle_client, args=(serviceSock, addr))
     client_thread.start()
